596cbb7c1104b1a374c2a77fcd13b6b7bd1dc651
ZhijiunY <yzj90596@gmail.com>
2023-03-01 00:21:15 +0800

    golang tmpl

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -28,6 +28,12 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals", handlers.Repo.Generals)
+	mux.Get("/majors", handlers.Repo.Majors)
+	mux.Get("/availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
