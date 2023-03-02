b98df95c4efe0ed0b6ff5fd5e08e014e8799feb9
ZhijiunY <yzj90596@gmail.com>
2023-03-02 14:29:07 +0800

    PostRepository

--- cmd/internal/handlers/handlers.go
+++ cmd/internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/ZhijiunY/booking-web/cmd/internal/config"
+	"github.com/ZhijiunY/booking-web/cmd/internal/forms"
 	"github.com/ZhijiunY/booking-web/cmd/internal/models"
 	"github.com/ZhijiunY/booking-web/cmd/internal/render"
 )
@@ -62,7 +63,14 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Generals renders the room page
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -38,6 +38,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/reservation", handlers.Repo.Reservation)
+	mux.Post("/reservation", handlers.Repo.PostReservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
