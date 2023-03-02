ba29fa90cdef5820a625aaf82cd6bc27f82b59a6
ZhijiunY <yzj90596@gmail.com>
2023-03-02 23:34:35 +0800

    Writing tests for main package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,26 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	// _ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	// what am I going to put in the session
 	// reservation-summary
 	gob.Register(models.Reservation{})
@@ -40,6 +60,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -50,16 +71,5 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
-	// _ = http.ListenAndServe(portNumber, nil)
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -9,4 +9,7 @@ require (
 
 require github.com/alexedwards/scs/v2 v2.5.0
 
-require github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/go-chi/chi v1.5.4
+)
